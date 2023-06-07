9948eec5b9a7842bd29dd98ffbd1d3a523a51dd1
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-06-07 17:17:19 +0530

    Project Add

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import Boards from "./Components/Boards"
+import Square from "./Components/Square"
+
+import "./Styles/styles.scss"
+
+function App() {
+
+
+  return (
+    <div>
+     
+      <Square/>
+      <Boards/>
+      
+    </div>
+  )
+}
+
+export default App
