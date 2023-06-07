9d213323bb9f9c064977971870dfdad069daf9fc
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-06-08 00:08:40 +0530

    board and square added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
 
+import { useState } from "react"
 import Boards from "./Components/Boards"
-import Square from "./Components/Square"
+
 
 import "./Styles/styles.scss"
 
 function App() {
 
 
+  //in react we can't use like javascript we need to defined state
+ //concept called re-renders : when the component repainted on the webpage
+ //use state always triggers a componemt to renders
+// render is isolate they don't know about other render
+//import useState  
+  // let counter =1;
+
+  // const onBtnClick =()=>{
+  //   console.log('hello');
+  //   counter=counter+1;
+  // };
+const [counter, setCounter]=useState(1);
+console.log('hello');
+const onBtnClick =()=>{
+   setCounter(currentCounter =>{
+    return currentCounter+1;
+   });
+  };
+
   return (
-    <div>
-     
-      <Square/>
-      <Boards/>
+    <div className="app">
+      <div>
+        <button onClick={
+          onBtnClick
+        }>Click me please</button>
+        <div>{counter}</div>
+      </div>
       
+        
     </div>
   )
 }
